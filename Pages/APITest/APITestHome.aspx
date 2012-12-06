c3f0b49c73b7e813a4a5a574cbb694e326c09720
Brandon Grant <BGrant@dev-2.RBSCRP.LOCAL>
2012-12-05 14:55:22 -1000

    - Created branch on web application for testing the api for asset tracker.   Will be used to ensure authorization and authentication are correctly   implemnented.

---
+++ Pages/APITest/APITestHome.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AssetTracker.Master" AutoEventWireup="true" CodeBehind="APITestHome.aspx.cs" Inherits="asset_tracker_web.Pages.APITest.APITestHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <p>The returned content from some stuff I wrote for this thing.</p>
+    <br /><br />
+    <asp:Label runat="server" ID="responseContent"></asp:Label>
+
+
+
+</asp:Content>
---
+++ Pages/APITest/APITestHome.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+
+namespace asset_tracker_web.Pages.APITest
+{
+    public partial class APITestHome : System.Web.UI.Page
+    {
+        private RestClient client = new RestClient("http://localhost:49725");
+        private string test = "<tag>test</tag>";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            client.Authenticator = new HttpBasicAuthenticator("rbstester", "rbscrp");
+            RestRequest request = new RestRequest("/api/Asset", Method.GET);
+            request.AddHeader("Accept", "text/xml");
+            request.RequestFormat = DataFormat.Xml;
+            var response = client.Execute(request);
+            responseContent.Text = response.Content;
+
+        }
+    }
+}
